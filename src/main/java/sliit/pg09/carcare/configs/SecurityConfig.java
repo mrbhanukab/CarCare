3ad0be6ff3090bb476a60efe1cb484c8b064c48a
mrbhanuka <mrbhanuka@outlook.com>
2025-04-06 12:46:07 +0530

    Successfully Setup Google OAuth2 Client

--- src/main/java/sliit/pg09/carcare/configs/SecurityConfig.java
+++ src/main/java/sliit/pg09/carcare/configs/SecurityConfig.java
@@ -2,7 +2,6 @@ package sliit.pg09.carcare.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,9 +11,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(request -> {
+                    request.requestMatchers("/").permitAll();
+                    request.requestMatchers("/scripts/*").permitAll();
+                    request.requestMatchers("/css/*").permitAll();
+                    request.requestMatchers("/images/**").permitAll();
+                    request.requestMatchers("/next-image").permitAll();
+                    request.anyRequest().authenticated();
+                })
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/")
+                        .successHandler((request, response, authentication) -> {
+                            response.sendRedirect("/client");
+                        })
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
                 .build();
     }
 }
