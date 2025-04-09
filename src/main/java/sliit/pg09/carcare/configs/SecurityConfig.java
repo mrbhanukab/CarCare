c3d0d88245619ffe86350da756325b2c91326e2c
mrbhanuka <mrbhanuka@outlook.com>
2025-04-09 12:05:47 +0530

    Refactor ClientController and SecurityConfig for improved user management and authentication handling

--- src/main/java/sliit/pg09/carcare/configs/SecurityConfig.java
+++ src/main/java/sliit/pg09/carcare/configs/SecurityConfig.java
@@ -1,24 +1,36 @@
 package sliit.pg09.carcare.configs;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import sliit.pg09.carcare.repository.ClientRepository;
+
+import java.io.IOException;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(request -> {
-                    request
-                            .requestMatchers("/", "/scripts/*", "/css/*", "/images/**", "/next-image").permitAll()
-                            .anyRequest().authenticated();
-                })
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/", "/scripts/**", "/css/**", "/images/**", "/next-image/**").permitAll()
+                        .anyRequest().authenticated())
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("/")
-                        .defaultSuccessUrl("/client")
+                        .successHandler(customAuthenticationSuccessHandler)
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
@@ -26,7 +38,29 @@ public class SecurityConfig {
                         .invalidateHttpSession(true)
                         .deleteCookies("JSESSIONID")
                 )
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
                 .build();
+    }
+
+    @Component
+    static class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+        @Autowired
+        ClientRepository clients;
+
+        @Override
+
+        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+
+            var user = ((OAuth2User) authentication.getPrincipal()).getAttributes();
+
+            clients.findById(user.get("email").toString()).ifPresentOrElse(u -> {
+            }, () -> clients.save(new sliit.pg09.carcare.model.Client(
+                    user.get("name").toString(),
+                    user.get("email").toString(),
+                    user.get("picture").toString()
+            )));
+            response.sendRedirect("/client/dashboard");
+        }
     }
 }
--- src/main/java/sliit/pg09/carcare/controller/ClientController.java
+++ src/main/java/sliit/pg09/carcare/controller/ClientController.java
@@ -7,8 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import sliit.pg09.carcare.model.Client;
 import sliit.pg09.carcare.repository.ClientRepository;
+
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/client")
@@ -17,52 +18,50 @@ public class ClientController {
     @Autowired
     ClientRepository clients;
 
+    private String manageUser(String file, Model model) {
+        var userEmail = (((OAuth2User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getAttributes()).get("email").toString();
+        return clients.findById(userEmail).map(user -> {
+            model.addAttribute("user", user);
+            if (user.getNic() == null && !Objects.equals(file, "Client/account")) {
+                return "redirect:/client/account";
+            }
+            return file;
+        }).orElse("redirect:/");
+    }
+
     @GetMapping("/dashboard")
     public String getDashboard(Model model) {
         model.addAttribute("active", "Dashboard");
-
-        var x = ((OAuth2User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getAttributes();
-
-        clients.findById(x.get("email").toString()).ifPresentOrElse(u -> model.addAttribute("user", u), () -> {
-            model.addAttribute("user",
-                    clients.save(new Client(
-                            x.get("name").toString(),
-                            x.get("email").toString(),
-                            x.get("picture").toString()
-                    ))
-            );
-        });
-
-        return "Client/dashboard";
+        return manageUser("Client/dashboard", model);
     }
 
     @GetMapping("/garage")
     public String getGarage(Model model) {
         model.addAttribute("active", "Garage");
-        return "Client/garage";
+        return manageUser("Client/garage", model);
     }
 
     @GetMapping("/schedule")
     public String getSchedule(Model model) {
         model.addAttribute("active", "Schedule");
-        return "Client/schedule";
+        return manageUser("Client/schedule", model);
     }
 
     @GetMapping("/history")
     public String getHistory(Model model) {
         model.addAttribute("active", "History");
-        return "Client/history";
+        return manageUser("Client/history", model);
     }
 
     @GetMapping("/vip-contact")
     public String getVipContact(Model model) {
         model.addAttribute("active", "VIP Contact");
-        return "Client/vip-contact";
+        return manageUser("Client/vip-contact", model);
     }
 
     @GetMapping("/account")
     public String getAccount(Model model) {
         model.addAttribute("active", "Account");
-        return "Client/account";
+        return manageUser("Client/account", model);
     }
 }
