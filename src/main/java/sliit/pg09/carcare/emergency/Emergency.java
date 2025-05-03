4dbf27a9c7115efdde9b115ee4fbcd9f969a42f6
IT24103814 <it24103814@my.sliit.lk>
2025-05-03 12:48:23 +0530

    Enhance EmergencyService to utilize VehicleService for vehicle retrieval and update Emergency entity with AllArgsConstructor

--- src/main/java/sliit/pg09/carcare/emergency/Emergency.java
+++ src/main/java/sliit/pg09/carcare/emergency/Emergency.java
@@ -1,6 +1,7 @@
 package sliit.pg09.carcare.emergency;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import sliit.pg09.carcare.vehicle.Vehicle;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Emergency {
     @EmbeddedId
--- src/main/java/sliit/pg09/carcare/emergency/EmergencyService.java
+++ src/main/java/sliit/pg09/carcare/emergency/EmergencyService.java
@@ -1,24 +1,29 @@
 package sliit.pg09.carcare.emergency;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import sliit.pg09.carcare.vehicle.Vehicle;
+import sliit.pg09.carcare.vehicle.VehicleService;
 
 import java.time.LocalDateTime;
 
 @Service
 public class EmergencyService {
     private final EmergencyRepository emergencyRepository;
+    private final VehicleService vehicleService;
 
-    public EmergencyService(EmergencyRepository emergencyRepository) {
+    @Autowired
+    public EmergencyService(EmergencyRepository emergencyRepository, VehicleService vehicleService) {
         this.emergencyRepository = emergencyRepository;
+        this.vehicleService = vehicleService;
     }
 
-    public void createEmergency(String description, String location, String vehicleLicence) {
-        Vehicle vehicle = new Vehicle();
-        vehicle.setLicense(vehicleLicence);
+    public void createEmergency(String location, String vehicleLicence) {
+        Vehicle vehicle = vehicleService.findVehicle(vehicleLicence);
 
         Emergency emergency = new Emergency(vehicle, LocalDateTime.now());
+        emergency.setLocation(location);
 
+        emergencyRepository.save(emergency);
     }
-
 }
