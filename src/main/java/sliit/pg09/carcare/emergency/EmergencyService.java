ff6429a9b2622752044ab2a35d1c7e97a4245e38
IT24103814 <it24103814@my.sliit.lk>
2025-05-03 12:06:50 +0530

    Add createEmergency method to EmergencyService for emergency creation

--- src/main/java/sliit/pg09/carcare/emergency/EmergencyService.java
+++ src/main/java/sliit/pg09/carcare/emergency/EmergencyService.java
@@ -1,6 +1,9 @@
 package sliit.pg09.carcare.emergency;
 
 import org.springframework.stereotype.Service;
+import sliit.pg09.carcare.vehicle.Vehicle;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmergencyService {
@@ -9,4 +12,13 @@ public class EmergencyService {
     public EmergencyService(EmergencyRepository emergencyRepository) {
         this.emergencyRepository = emergencyRepository;
     }
+
+    public void createEmergency(String description, String location, String vehicleLicence) {
+        Vehicle vehicle = new Vehicle();
+        vehicle.setLicense(vehicleLicence);
+
+        Emergency emergency = new Emergency(vehicle, LocalDateTime.now());
+
+    }
+
 }
