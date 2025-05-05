6bd91ab1dd9bbaf62bbe3f25819c2c051a7fd69c
IT24103814 <it24103814@my.sliit.lk>
2025-05-05 13:47:18 +0530

    Add ongoing appointment retrieval functionality and enhance service integration

--- src/main/java/sliit/pg09/carcare/ongoingAppointment/OngoingAppointmentController.java
+++ src/main/java/sliit/pg09/carcare/ongoingAppointment/OngoingAppointmentController.java
@@ -1,10 +1,27 @@
 package sliit.pg09.carcare.ongoingAppointment;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class OngoingAppointmentController {
     @Autowired
-    OngoingAppointmentService ongoingAppointmentService;
+    static OngoingAppointmentService ongoingAppointmentService;
+
+    @RequestMapping("/client")
+    public static class ClientOngoingAppointment {
+        @GetMapping("/ongoing-appointment")
+        public ResponseEntity<List<OngoingAppointment>> getOngoingAppointment(
+                @RequestParam String vehicle) {
+            List<OngoingAppointment> ongoingAppointments = ongoingAppointmentService.getOngoingAppointments(vehicle);
+            return ResponseEntity.ok(ongoingAppointments);
+        }
+
+    }
 }
