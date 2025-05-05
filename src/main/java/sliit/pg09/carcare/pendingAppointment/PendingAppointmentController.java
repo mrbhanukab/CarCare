8d548f48754c4cb51d4dac7b5579097c5ccd016c
IT24103814 <it24103814@my.sliit.lk>
2025-05-05 11:14:48 +0530

    Implement pending appointment retrieval for clients and enhance service layer

--- src/main/java/sliit/pg09/carcare/pendingAppointment/PendingAppointmentController.java
+++ src/main/java/sliit/pg09/carcare/pendingAppointment/PendingAppointmentController.java
@@ -1,10 +1,30 @@
 package sliit.pg09.carcare.pendingAppointment;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class PendingAppointmentController {
-    @Autowired
     PendingAppointmentService pendingAppointmentService;
+
+
+    @RequestMapping("/client")
+    public static class ClientPendingAppointment {
+        @Autowired
+        private PendingAppointmentService pendingAppointmentService;
+
+        @GetMapping("/pending-appointment")
+        public ResponseEntity<List<PendingAppointment>> getPendingAppointment(
+                @RequestParam String vehicle) {
+            List<PendingAppointment> pendingAppointments = pendingAppointmentService.getPendingAppointments(vehicle);
+            return ResponseEntity.ok(pendingAppointments);
+        }
+
+    }
 }
--- src/main/java/sliit/pg09/carcare/pendingAppointment/PendingAppointmentService.java
+++ src/main/java/sliit/pg09/carcare/pendingAppointment/PendingAppointmentService.java
@@ -1,12 +1,20 @@
 package sliit.pg09.carcare.pendingAppointment;
 
 import org.springframework.stereotype.Service;
+import sliit.pg09.carcare.vehicle.VehicleRepository;
+
+import java.util.List;
 
 @Service
 public class PendingAppointmentService {
     private final PendingAppointmentRepository pendingAppointmentRepository;
+    private final ve;
 
-    public PendingAppointmentService(PendingAppointmentRepository pendingAppointmentRepository) {
+    public PendingAppointmentService(PendingAppointmentRepository pendingAppointmentRepository, VehicleRepository vehicleRepository) {
         this.pendingAppointmentRepository = pendingAppointmentRepository;
+    }
+
+    public List<PendingAppointment> getPendingAppointments(String vehicle) {
+        return pendingAppointmentRepository.findByVehicle(vehicleRepository.)
     }
 }
