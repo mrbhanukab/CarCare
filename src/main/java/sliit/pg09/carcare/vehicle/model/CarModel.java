998958a4e54248627bddfd9accc91c182b00fdd7
mrbhanuka <mrbhanuka@outlook.com>
2025-05-09 21:34:22 +0530

    Add CarModel entity and revamp admin dashboard UI
    Introduced `CarModel` entity in the backend with JPA annotations for vehicle data management. Updated the admin dashboard layout to use a new modular navigation component and streamlined UI using Tailwind CSS. Adjusted HTML and CSS for a cleaner, more dynamic user experience.

---
+++ src/main/java/sliit/pg09/carcare/vehicle/model/CarModel.java
@@ -0,0 +1,52 @@
+package sliit.pg09.carcare.vehicle.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import sliit.pg09.carcare.vehicle.Vehicle;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class CarModel {
+
+    @Id
+    private String number;
+    private color color;
+    private type type;
+    private int year;
+    private int noOfCylinders;
+    private int power;
+    private double length;
+    private double width;
+    private double wheelBase;
+    private String make;
+    private String image;
+    @OneToMany(mappedBy = "model", cascade = CascadeType.ALL)
+    private List<Vehicle> vehicles;
+
+    public CarModel(String number, color color, type type, int year, int noOfCylinders, int power, double length, double width, double wheelBase) {
+        this.number = number;
+        this.color = color;
+        this.type = type;
+        this.year = year;
+        this.noOfCylinders = noOfCylinders;
+        this.power = power;
+        this.length = length;
+        this.width = width;
+        this.wheelBase = wheelBase;
+    }
+
+    public enum color {
+        RED, BLUE, GREEN, YELLOW, BLACK, WHITE
+    }
+
+    public enum type {
+        SEDAN, SUV, TRUCK, COUPE, HATCHBACK
+    }
+}
