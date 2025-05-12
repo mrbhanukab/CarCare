ef6247b075362c43a1e4b6ee10d7ed3f6da00b09
mrbhanuka <mrbhanuka@outlook.com>
2025-05-12 15:38:48 +0530

    Refactor Admin dashboards to rename and restructure files
    Renamed and restructured Admin screen templates for better clarity, replacing "UsersDashboard.html" with "Clients.html" and "LogDashboard.html" with "LogsDashboard.html". This improves the naming convention and aligns with planned updates to the Admin interface.

--- src/main/java/sliit/pg09/carcare/admin/AdminController.java
+++ src/main/java/sliit/pg09/carcare/admin/AdminController.java
@@ -1,5 +1,6 @@
 package sliit.pg09.carcare.admin;
 
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +16,42 @@ public class AdminController {
     @RequestMapping(value = {"", "/"})
     public String getDashboard(Model model) {
         return adminService.verifyUserStatus("Admin/Dashboard", model);
+    }
+
+    @HxRequest
+    @GetMapping("/screen/emergencies")
+    public String getEmergencyScreen(Model model) {
+        return adminService.verifyUserStatus("Admin/Screens/EmergenciesDashboard", model);
+    }
+
+    @HxRequest
+    @GetMapping("/screen/appointments-requests")
+    public String getAppointmentsRequests(Model model) {
+        return adminService.verifyUserStatus("Admin/Screens/AppointmentsDashboard", model);
+    }
+
+    @HxRequest
+    @GetMapping("/screen/ongoing-appointments")
+    public String getOngoingAppointmentsRequests(Model model) {
+        return adminService.verifyUserStatus("Admin/Screens/OngoingDashboard", model);
+    }
+
+    @HxRequest
+    @GetMapping("/screen/completed-appointments")
+    public String getCompletedAppointmentsRequests(Model model) {
+        return adminService.verifyUserStatus("Admin/Screens/CompletedDashboard", model);
+    }
+
+    @HxRequest
+    @GetMapping("/screen/log")
+    public String getLogs(Model model) {
+        return adminService.verifyUserStatus("Admin/Screens/LogsDashboard", model);
+    }
+
+    @HxRequest
+    @GetMapping("/screen/clients")
+    public String getClients(Model model) {
+        return adminService.verifyUserStatus("Admin/Screens/Clients", model);
     }
 
     @GetMapping("/login")
